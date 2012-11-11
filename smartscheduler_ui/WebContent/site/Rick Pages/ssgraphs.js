0d056ded48f8af85246a92f0ef6e279ca17169d9
rikx <ricardo.fuentes@upr.edu>
2012-11-11 01:20:41 -0400

    Added a bar graph, using High Charts, to make the Time Management Techniques page more interactive.
    Signed-off-by: rikx <ricardo.fuentes@upr.edu>

---
+++ smartscheduler_ui/WebContent/site/Rick Pages/ssgraphs.js
@@ -0,0 +1,53 @@
+$(function () {
+    var chart;
+    $(document).ready(function() {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'container',
+                type: 'bar'
+            },
+            title: {
+                text: 'Pomodoro Cycles'
+            },
+            xAxis: {
+                categories: ['First cycle', 'Second cycle', 'Third cycle', 'Fourth cycle']
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Time (minutes)'
+                }
+            },
+            legend: {
+                backgroundColor: '#FFFFFF',
+                reversed: true
+            },
+            tooltip: {
+                formatter: function() {
+                  if(this.series.name == 'Elapsed time so far')
+                    return 'Elapsed time after '+
+                        this.x.toLowerCase() +': '+ this.y +'';
+                  else
+                    return ''+
+                        this.series.name +': '+ this.y +'';
+                }
+            },
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            },
+                series: [{
+                name: 'Elapsed time so far',
+                data: [30, 60, 90, 140]
+            }, {
+                name: 'Rest period',
+                data: [5, 5, 5, 25]
+            }, {
+                name: 'Study period',
+                data: [25, 25, 25, 25]
+            }]
+        });
+    });
+    
+});
