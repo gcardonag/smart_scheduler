7f074fb5acc5554d77338b185ed8c85f5a2bd2e9
rikx <ricardo.fuentes@upr.edu>
2012-12-22 03:18:39 -0400

    Cleaned up some unused code on some of he pages, and fixed some errors. Also trying out some variations on the event manager page to see which one is better.

--- smartscheduler_ui/WebContent/site/Rick Pages/bootstrap prototypes/js/smartscheduler.js
+++ smartscheduler_ui/WebContent/site/Rick Pages/bootstrap prototypes/js/smartscheduler.js
@@ -1,3 +1,34 @@
-var signIn = function(){
-    alert("Test alert; please ignore");
+$(document).ready(function () {
+    //event handler for submit button
+    $("#signInBtn").click(function () {
+        //collect userName and password entered by users
+        var userName = $("#username").val();
+        var password = $("#password").val();
+
+        //call the authenticate function
+        authenticate(userName, password);
+    });
+});
+
+//authenticate function to make ajax call
+function authenticate(userName, password) {
+    $.ajax
+    ({
+        type: "POST",
+        //the url where you want to sent the userName and password to
+        //url: "http://your-url.com/secure/authenticate.php",
+        dataType: 'json',
+        async: false,
+        //json object to sent to the authentication url
+        data: '{"userName": "' + userName + '", "password" : "' + password + '"}',
+        success: function () {
+            //do any process for successful authentication here
+            alert("Welcome " + userName + "!");
+        }
+        alert("Server is down :(");
+    })
 }
+
+/*var signIn = function(){
+    alert("Test alert; please ignore");
+}*/
