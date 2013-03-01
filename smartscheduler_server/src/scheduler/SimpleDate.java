5389f6118103befa3dfee09486f1ac5d410cceff
allanos <anthony.llanos@upr.edu>
2013-03-01 18:27:47 -0400

    Event Collection Update
    + Updated the RecurrenceGroup Class with accessors for:
    
    int recurrence;
    boolean[] weekly;
    int interval;
    
    + Added method that determines whether an event occurs on a given
    weekday if the recurrence is set to weekly.
    
    - Deprecated containsTime() method from the Event class due to
    inconsistencies in use (so far, this is a JVM problem).
    
    + Added basic documentation just to make a starting point for this.

--- smartscheduler_server/src/scheduler/SimpleDate.java
+++ smartscheduler_server/src/scheduler/SimpleDate.java
@@ -4,6 +4,7 @@ import java.util.GregorianCalendar;
 
 public class SimpleDate extends GregorianCalendar {
 	
+	
 	public String toString() {
 		String month = super.getDisplayName(field, style, locale)
 		return str;
