6c5aef5e04a33debb55a1286235a1d84b0248104
Gabriel Cardona <gcardonagarcia@gmail.com>
2013-03-06 14:54:48 -0400

    Minor changes in HTML and EventObject JS

--- smartscheduler_ui/WebContent/site/test/EventObject.js
+++ smartscheduler_ui/WebContent/site/test/EventObject.js
@@ -1,6 +1,7 @@
 eventCounter = 0;
 function Event(name) {
 	this.name = name;
+	this.recurrance = false;
 };
 
 Event.setDates = function(startDate, endDate){
@@ -12,4 +13,11 @@ Event.setDates = function(startDate, endDate){
 Event.setTimes = function(startTime, endTime){
 	this.startTime = startTime;
 	this.endTime = endTime;
-}
+};
+
+Event.toggleRecurrance = function(){
+	if(recurrance_flag = true)
+		recurrance_flag = false;
+	else
+		recurrance_flag = true;
+};
