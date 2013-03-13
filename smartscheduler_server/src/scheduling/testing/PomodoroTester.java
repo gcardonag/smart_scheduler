6ae8b40559eccb506e5352267a89ebc300333fdc
Nelson Reyes <nelson.reyes@upr.edu>
2013-03-13 16:22:31 -0400

    Added some class testers

---
+++ smartscheduler_server/src/scheduling/testing/PomodoroTester.java
@@ -0,0 +1,56 @@
+package scheduling.testing;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import scheduler.PomodoroCreator;
+
+import eventCollection.*;
+
+public class PomodoroTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		EventCollection c = new EventCollection();
+		EventQueue q = new EventQueue();
+		PomodoroCreator p = new PomodoroCreator(25, 5, 30);
+		
+		Calendar start = new GregorianCalendar();
+		start.set(2013, Calendar.MARCH, 3, 8, 0);
+		
+		Calendar end = (Calendar) start.clone();
+		end.add(Calendar.HOUR_OF_DAY, 1);
+		
+		Event aclass = new Event("Class", start, end, true, true);
+		
+		start = (Calendar) end.clone();
+		end.add(Calendar.HOUR_OF_DAY, 2);
+		
+		Event studytime = new Event("Study", start, end, false, true);
+		
+		Calendar recend = (Calendar) start.clone();
+		recend.add(Calendar.DAY_OF_YEAR, 6);
+
+		boolean[] w = {false,true,false,true,false,true,false};
+		RecurrenceGroup classrec = new RecurrenceGroup(aclass, RecurrenceGroup.WEEKLY, 1, recend, w);
+		RecurrenceGroup studyrec = new RecurrenceGroup(studytime, RecurrenceGroup.WEEKLY, 1, recend, w);
+		
+		//SIMULATING SOME TIME MANAGEMENT ALGORITHM
+		for(Event e : classrec.getEventsInRecurrenceGroup())
+			c.add(e);
+		for(Event e : studyrec.getEventsInRecurrenceGroup())
+			q.insert(e);
+		while(!q.isEmpty()) {
+			Iterable<Event> pomodoros = p.getPomodorosFromEvent(q.removeMin());
+			for(Event e : pomodoros)
+				c.add(e);
+		}
+		for(Event e : c)
+			System.out.println(e.toString());
+		
+	}
+
+}
