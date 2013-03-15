035a4f630348b03244f7839419e95d9c3b25b355
rikx <ricardo.fuentes@upr.edu>
2013-03-15 04:25:17 -0400

    Light modifications to 'userschedule' page as part of ongoing jQuery tests. Added a 'test' page for similar purpose, and test jQuery has been added to the 'smartscheduler.js' file.

--- smartscheduler_ui/WebContent/site/Rick Pages/bootstrap prototypes/js/smartscheduler.js
+++ smartscheduler_ui/WebContent/site/Rick Pages/bootstrap prototypes/js/smartscheduler.js
@@ -1,4 +1,49 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    $('#submitBtn').click(function(){
+        var eventName = $('#eName').attr('value');
+        var startTime = $('#esTime').val();
+        var endTime = $('#eeTime').val();
+        var startDate = $('#esDate').val();
+        var endDate = $('#eeDate').val();
+       
+        $('#rightCol').append("<h1>Hello</h1>"                  
+
+        );
+/*
+        $('#rightCol').append(
+            "<div class='accordion' id='accordionEventsList'>
+              <div class='accordion-group'>
+                   <div class='accordion-heading'>
+                   <a class='accordion-toggle' data-toggle='collapse' data-parent='#accordionEventsList' href='#collapse" +$eventName+ "' style='background-color: #46a546'>
+                   <strong style='color:white'>" + $eventName + "</strong>
+                   </a>
+                 </div>
+                 <div id='collapse"+$eventName+ "' class='accordion-body collapse'>
+                    <div class='accordion-inner'>
+                        <dl class='dl-horizontal'>
+                            <dt>Name:</dt>
+                            <dd>" + $eventName + "</dd>
+                            <dt>Start Time:</dt>
+                            <dd>" + $startTime + "</dd>
+                            <dt>End Time:</dt>
+                            <dd>" + $endTime + "</dd>
+                            <dt>Start Date:</dt>
+                            <dd>" + $startDate + "</dd>
+                            <dt>End Date:</dt>
+                            <dd>" + $endDate + "</dd>
+                        </dl>
+                        <button class='btn btn-inverse'>Delete event</button>
+                    </div>
+                  </div><!-- End of collapseClass -->
+                </div><!-- End of Class accordion group -->
+            </div><!--End of accodion -->"
+        );
+*/
+
+    });
+});
+
+/*
     //event handler for submit button
     $("#signInBtn").click(function () {
         //collect userName and password entered by users
@@ -10,31 +55,31 @@ $(document).ready(function () {
         alert("OMG " + userName + ", your password is " + password);
     });
 
-        $("a").click(function(event){
+    $("a").click(function(event){
         event.preventDefault();
         $(this).hide("slow");
     });
-});
 
-//authenticate function to make ajax call
-function authenticate(userName, password) {
-    $.ajax
-    ({
-        type: "POST",
-        //the url where you want to sent the userName and password to
-        //url: "http://your-url.com/secure/authenticate.php",
-        dataType: 'json',
-        async: false,
-        //json object to sent to the authentication url
-        data: '{"userName": "' + userName + '", "password" : "' + password + '"}',
-        success: function () {
-            //do any process for successful authentication here
-            alert("Welcome " + userName + "!");
-        }
-        alert("Server is down :(");
-    })
-}
 
-/*var signIn = function(){
+    //authenticate function to make ajax call
+    function authenticate(userName, password) {
+        $.ajax
+        ({
+            type: "POST",
+            //the url where you want to sent the userName and password to
+            //url: "http://your-url.com/secure/authenticate.php",
+            dataType: 'json',
+            async: false,
+            //json object to sent to the authentication url
+            data: '{"userName": "' + userName + '", "password" : "' + password + '"}',
+            success: function () {
+                //do any process for successful authentication here
+                alert("Welcome " + userName + "!");
+            }
+            alert("Server is down :(");
+        });
+    }
+    var signIn = function(){
     alert("Test alert; please ignore");
-}*/
+}
+*/
