80a5f39ff2cf62bf98578daedffa251284af0880
rikx <ricardo.fuentes@upr.edu>
2013-03-15 21:00:08 -0400

    i friggin updated this shit beotch y u no work

--- smartscheduler_ui/WebContent/site/Rick Pages/bootstrap prototypes/js/smartscheduler.js
+++ smartscheduler_ui/WebContent/site/Rick Pages/bootstrap prototypes/js/smartscheduler.js
@@ -1,40 +1,44 @@
 $(document).ready(function() {
     $('#submitBtn').click(function(){
-        var eventName = $('#eName').attr('value');
+        var eventName = $('#eName').attr('value'); //its just an alternative to .val()
         var startTime = $('#esTime').val();
         var endTime = $('#eeTime').val();
         var startDate = $('#esDate').val();
         var endDate = $('#eeDate').val();
        
+       //This works but its too simple
+        $('#rightCol #accordionEventsList').append("<p>Name: " + eventName + "</p> <p>startTime: " + startTime + "</p>");
 
-        $('#rightCol #accordionEventsList').html(
-            "
-                <div class='accordion-group'>
-                    <div class='accordion-heading'>
-                        <a class='accordion-toggle' data-toggle='collapse' data-parent='#accordionEventsList' href='#collapse" +eventName+ "' style='background-color: #46a546'>
-                        <strong style='color:white'>" + eventName + "</strong>
-                        </a>
-                    </div>
-                    <div id='collapse"+eventName+ "' class='accordion-body collapse'>
-                        <div class='accordion-inner'>
-                            <dl class='dl-horizontal'>
-                                <dt>Name:</dt>
-                                <dd>" + eventName + "</dd>
-                                <dt>Start Time:</dt>
-                                <dd>" + startTime + "</dd>
-                                <dt>End Time:</dt>
-                                <dd>" + endTime + "</dd>
-                                <dt>Start Date:</dt>
-                                <dd>" + startDate + "</dd>
-                                <dt>End Date:</dt>
-                                <dd>" + endDate + "</dd>
-                            </dl>
-                            <button class='btn btn-inverse' id='deleteBtn'>Delete event</button>
-                        </div>
-                    </div>
-                </div>
-            "
-        );
+        //This does not work for some reason. This, if you look at it in conjunction with the test.html page, 
+        //should work like this: you add an event and it gets added as an extra collapsable element in the accordion
+        // $('#rightCol #accordionEventsList').append(
+        //     "
+        //         <div class='accordion-group'>
+        //             <div class='accordion-heading'>
+        //                 <a class='accordion-toggle' data-toggle='collapse' data-parent='#accordionEventsList' href='#collapse" +eventName+ "' style='background-color: #46a546'>
+        //                 <strong style='color:white'>" + eventName + "</strong>
+        //                 </a>
+        //             </div>
+        //             <div id='collapse"+eventName+ "' class='accordion-body collapse'>
+        //                 <div class='accordion-inner'>
+        //                     <dl class='dl-horizontal'>
+        //                         <dt>Name:</dt>
+        //                         <dd>" + eventName + "</dd>
+        //                         <dt>Start Time:</dt>
+        //                         <dd>" + startTime + "</dd>
+        //                         <dt>End Time:</dt>
+        //                         <dd>" + endTime + "</dd>
+        //                         <dt>Start Date:</dt>
+        //                         <dd>" + startDate + "</dd>
+        //                         <dt>End Date:</dt>
+        //                         <dd>" + endDate + "</dd>
+        //                     </dl>
+        //                     <button class='btn btn-inverse' id='deleteBtn'>Delete event</button>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     "
+        // );
     });
 
     $('#deleteBtn').click(function(){
