37a58e4d5fb83b70ca6f81ac6448c8db62d97b8c
rikx <ricardo.fuentes@upr.edu>
2013-03-21 16:43:35 -0400

    working on an alternative version of the event manager page.

--- smartscheduler_ui/WebContent/site/Rick Pages/bootstrap prototypes/js/smartschedulertest.js
+++ smartscheduler_ui/WebContent/site/Rick Pages/bootstrap prototypes/js/smartschedulertest.js
@@ -71,8 +71,54 @@ $(document).ready(function() {
          );
     });
 
-    $('#deleteBtn').live('click', function(){
+
+    $(document).on('click', '#deleteBtn', function() {
         $(this).parents().eq(2).remove();
     });
 });
 
+/* ALTERNATE DELETE   
+    $('#deleteBtn').live('click', function(){
+        $(this).parents().eq(2).remove();
+    });*/
+
+/*
+    //event handler for submit button
+    $("#signInBtn").click(function () {
+        //collect userName and password entered by users
+        var userName = $("#username").val();
+        var password = $("#password").val();
+
+        //call the authenticate function
+        //authenticate(userName, password);
+        alert("OMG " + userName + ", your password is " + password);
+    });
+
+    $("a").click(function(event){
+        event.preventDefault();
+        $(this).hide("slow");
+    });
+
+
+    //authenticate function to make ajax call
+    function authenticate(userName, password) {
+        $.ajax
+        ({
+            type: "POST",
+            //the url where you want to sent the userName and password to
+            //url: "http://your-url.com/secure/authenticate.php",
+            dataType: 'json',
+            async: false,
+            //json object to sent to the authentication url
+            data: '{"userName": "' + userName + '", "password" : "' + password + '"}',
+            success: function () {
+                //do any process for successful authentication here
+                alert("Welcome " + userName + "!");
+            }
+            alert("Server is down :(");
+        });
+    }
+    var signIn = function(){
+    alert("Test alert; please ignore");
+}
+*/
