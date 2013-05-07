75a5c861649d485e49312b62be6c3becf95fe1f7
Gabriel Cardona <gcardonagarcia@gmail.com>
2013-05-06 23:55:07 -0400

    SQL INSERT and SELECT tests.
    -Its been so long...

--- smartscheduler_prototype/src/database/DBTest.java
+++ smartscheduler_prototype/src/database/DBTest.java
@@ -1,14 +1,18 @@
 package database;
 import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBTest {
 
 	/**
 	 * @param args
+	 * @throws SQLException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		System.out.println("-------- PostgreSQL "
 				+ "JDBC Connection Testing ------------");
  
@@ -46,6 +50,23 @@ public class DBTest {
  
 		if (connection != null) {
 			System.out.println("You made it, take control your database now!");
+			Statement st = connection.createStatement();
+			ResultSet rs = st.executeQuery("SELECT * FROM \"Events\" WHERE id = 2");
+			while (rs.next()) {
+			    System.out.print("Column 1 returned ");
+			    System.out.println(rs.getString(1));
+			    System.out.println(rs.getString(2));
+			    System.out.println(rs.getString(3));
+			}
+			rs.close();
+			st.close();
+			
+			String foovalue = null;
+			PreparedStatement st2 = connection.prepareStatement("INSERT INTO \"Events\" VALUES (DEFAULT,?, 'class')");
+			st2.setString(1, foovalue);
+			st2.executeUpdate();
+			System.out.println("Added stuff.");
+			st2.close();
 		} else {
 			System.out.println("Failed to make connection!");
 		}
